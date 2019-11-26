7f83621c78915ebf6b92e3ec2ebe551c16c6ea36
Brandon Lowe <brandon.lowe615@gmail.com>
2019-11-26 14:50:50 -0600

    feat: Create app directory with run.py file
    run.py gathers the data, model, visualizations, and html files before
    launching the web app

---
+++ app/run.py
@@ -0,0 +1,19 @@
+# import libraries
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# load data
+
+# load model
+
+# define functions
+
+# define routes
+# index
+
+# go
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3001, debug=True)
