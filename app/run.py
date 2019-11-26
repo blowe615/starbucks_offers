2075ae81ac52d5071d5c04e219bccc6821414e50
Brandon Lowe <brandon.lowe615@gmail.com>
2019-11-26 17:07:23 -0600

    feat: Add index route to web app that displays figures

--- app/run.py
+++ app/run.py
@@ -1,18 +1,33 @@
 # import libraries
+import json, plotly
 from flask import Flask
+from flask import render_template
+from process_data import return_figures
+
 
 app = Flask(__name__)
 
 
-# load data
-
-# load model
-
-# define functions
-
 # define routes
-# index
+@app.route('/')
+@app.route('/index')
+def index():
 
+    figures = return_figures()
+
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.html',
+                           ids=ids,
+                           figuresJSON=figuresJSON)
+
+# web page that handles user query and displays model results
+# @app.route('/go')
+# def go():
 # go
 
 if __name__ == '__main__':
