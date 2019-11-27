46da517deeb55fafeb182f59b9043aee17637bce
Brandon Lowe <brandon.lowe615@gmail.com>
2019-11-26 22:52:52 -0600

    feat: Add go route to flask app to make new reward recommendations

--- app/run.py
+++ app/run.py
@@ -1,5 +1,7 @@
 # import libraries
+import numpy as np
 import json, plotly
+from datetime import datetime
 from flask import Flask
 from flask import render_template
 from process_data import return_figures
@@ -27,7 +29,24 @@ def index():
 
 # web page that handles user query and displays model results
 # @app.route('/go')
-# def go():
+def go():
+    # save user inputs
+    # query = request.args.get('query', '')
+    age = request.args.get('age', np.nan)
+    income = request.args.get('income', np.nan)
+    enrollment_date = request.args.get('enrollment_date', datetime(2017,1,1))
+    gender = request.args.get('gender', '')
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file.
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
 # go
 
 if __name__ == '__main__':
